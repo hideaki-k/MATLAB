3379f659c5b58c88a627f5ad16c669ce2f0c5008
hideaki-k <h.kinoshita@kimura-lab.net>
2020-12-11 23:48:34 +0900

    Update pnh_imtool.m

--- M1/asobi/pnh_imtool.m
+++ M1/asobi/pnh_imtool.m
@@ -1,1 +1,4 @@
-imtool('imgsh_20081102T071400_wm8_fp_l_133.png');
+I = imread('sh_20081102T071400_wm8_fp_l_524.png');
+C = imread('imgsh_20081102T071400_wm8_fp_l_524.png');
+B = imfuse(I,C,'blend','Scaling','joint');
+imshow(B);
